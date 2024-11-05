7f693e095f642e0b874590e25c10aaa6fb25bb32
21-Raghav <21raghavsaxena@gmail.com>
2024-11-05 17:37:11 +0530

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:5000',
+//     },
+//   },
+//   plugins: [react()],
+//   base: "https://21-raghav.github.io/Lawyer-Page/",
+// });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': {
+        target: 'https://21-raghav.github.io',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyRes', (proxyRes) => {
+            proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+          });
+        },
+      },
     },
   },
-  plugins: [react()],
-  base: "https://21-raghav.github.io/Lawyer-Page/",
-})
+});
