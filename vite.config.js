32ef1fb9d400a0292b0232ecac158381fe6ec8b8
21-Raghav <21raghavsaxena@gmail.com>
2024-12-02 16:42:19 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,35 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/api': 'http://localhost:5000',
-//     },
-//   },
-//   plugins: [react()],
-//   base: "https://21-raghav.github.io/Lawyer-Page/",
-// });
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
-  
   server: {
     proxy: {
-      '/api': {
-        target: 'https://21-raghav.github.io/Lawyer-Page/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        configure: (proxy) => {
-          proxy.on('proxyRes', (proxyRes) => {
-            proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-          });
-        },
-      },
+      '/api': 'http://localhost:5000',
     },
   },
   plugins: [react()],
   base: "https://21-raghav.github.io/Lawyer-Page/",
 });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+  
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://21-raghav.github.io/Lawyer-Page/',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//         configure: (proxy) => {
+//           proxy.on('proxyRes', (proxyRes) => {
+//             proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//           });
+//         },
+//       },
+//     },
+//   },
+//   plugins: [react()],
+//   base: "https://21-raghav.github.io/Lawyer-Page/",
+// });
