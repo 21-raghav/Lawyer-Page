c57e1a5d30ab090b1ce0cd3441bed28bfdb13da8
21-Raghav <21raghavsaxena@gmail.com>
2024-12-02 16:35:04 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  
   server: {
     proxy: {
       '/api': {
@@ -30,4 +30,6 @@ export default defineConfig({
       },
     },
   },
+  plugins: [react()],
+  base: "https://21-raghav.github.io/Lawyer-Page/",
 });
