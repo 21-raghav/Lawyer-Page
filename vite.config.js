c8ab5325614ea9b2cf8a6e5d44253b3b3365b392
21-Raghav <21raghavsaxena@gmail.com>
2024-12-02 16:39:35 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://21-raghav.github.io',
+        target: 'https://21-raghav.github.io/Lawyer-Page/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         configure: (proxy) => {
